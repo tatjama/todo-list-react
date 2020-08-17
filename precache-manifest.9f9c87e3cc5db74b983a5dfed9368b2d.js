1984dd62dd7bdf8c58ae25f930c40f8e9d039c8c
tatjama <tanja120a@gmail.com>
2020-08-18 01:10:02 +0200

    Updates

---
+++ precache-manifest.9f9c87e3cc5db74b983a5dfed9368b2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70a1715f5d46032c9d0f2d958a687afc",
+    "url": "/github.io/todo-list-react/index.html"
+  },
+  {
+    "revision": "be0b9879aaf3329afa08",
+    "url": "/github.io/todo-list-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fb4cae775f9cbb159b5a",
+    "url": "/github.io/todo-list-react/static/js/2.028635af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/todo-list-react/static/js/2.028635af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be0b9879aaf3329afa08",
+    "url": "/github.io/todo-list-react/static/js/main.8b3f0426.chunk.js"
+  },
+  {
+    "revision": "bb76b6f8a15300f971a9",
+    "url": "/github.io/todo-list-react/static/js/runtime-main.cd4189db.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github.io/todo-list-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
