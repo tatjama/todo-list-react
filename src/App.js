c7fddc65c352a2af46b0e46dcddb99025f90331f
tatjama <tanja120a@gmail.com>
2020-08-18 12:09:37 +0200

    handleSubmit and addTask created import library nanoid for creating unique id to tasks

--- src/App.js
+++ src/App.js
@@ -1,38 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {nanoid} from 'nanoid';
 import ToDo from './components/ToDo';
 import Form from './components/Form';
 import FilterButton from './components/FilterButton';
 
 function App(props) {
 
-  const tasks = props.tasks.map((task)=>{
+  const [tasks, setTasks] = useState(props.tasks);
+
+  const taskList = tasks.map((task)=>{
     return <ToDo 
-      name = {task.name}
       id = {task.id}
+      name = {task.name}      
       completed = {task.completed}
       key = {task.id}
     />
     
   }) 
 
+  
+
+  function addTask(name) {
+    const newTask = {id: "todo-" + nanoid(), name: name, completed: false};
+    console.log(newTask)
+    setTasks([...tasks, newTask]);  
+    console.log(tasks)  
+  }
+
+  
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form/>
+      <Form addTask = {addTask}/>
       <div className="filters btn-group stack-exception">
        <FilterButton name = "all" pressed = "true"/>
        <FilterButton name = "Active" pressed = "false"/>
        <FilterButton name = "Completed" pressed = "false"/>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+        
       </div>
       <h2 id="list-heading">
         3 tasks remaining
@@ -42,7 +47,7 @@ function App(props) {
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-       {tasks}
+       {taskList}
       </ul>
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,26 @@
-import React from 'react';
-export default function Form() {
+import React, {useState} from 'react';
+
+export default function Form(props) {
+
+    const [name, setName] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+         
+        alert("form submit value is " + name);
+        props.addTask(name)
+        setName("");
+    }
+function handleChange(e) {
+    console.log(e.target.value);    
+    const newTask = e.target.value;
+    setName(newTask);
+    console.log(name);
+
+}
+
+
+
     return(
         <form>
         <h2 className="label-wrapper">
@@ -13,8 +34,14 @@ export default function Form() {
           className="input input__lg"
           name="text"
           autoComplete="off"
+          onChange = {handleChange}
+          value = {name}
         />
-        <button type="submit" className="btn btn__primary btn__lg">
+        <button 
+            type="submit" 
+            className="btn btn__primary btn__lg"
+            onClick = {handleSubmit}
+        >
           Add
         </button>
       </form>
