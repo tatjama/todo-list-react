80954a3717dde00d56248c18f8ea229a31826574
tatjama <tanja120a@gmail.com>
2020-08-18 06:59:53 +0200

    Created components FilterButton.js and Form.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
 import React from 'react';
 import ToDo from './components/ToDo';
+import Form from './components/Form';
+import FilterButton from './components/FilterButton';
 
 function App(props) {
+
+  const tasks = props.tasks.map((task)=>{
+    return <ToDo 
+      name = {task.name}
+      id = {task.id}
+      completed = {task.completed}
+      key = {task.id}
+    />
+    
+  }) 
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+      <Form/>
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+       <FilterButton name = "all" pressed = "true"/>
+       <FilterButton name = "Active" pressed = "false"/>
+       <FilterButton name = "Completed" pressed = "false"/>
         <button type="button" className="btn toggle-btn" aria-pressed="false">
           <span className="visually-hidden">Show </span>
           <span>Active</span>
@@ -47,9 +42,7 @@ function App(props) {
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-        <ToDo name = "Eat" completed = {true} id = "todo-0"/>
-       <ToDo name = "Sleep" completed = {false} id = "todo-1"/>
-       <ToDo name = "Repeat"completed = {false} id ="todo-2"/>
+       {tasks}
       </ul>
     </div>
   );
